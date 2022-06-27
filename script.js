5c423ed40ede537235d7dc982f4e7b0fbcce61a6
BrandtD <brandtdavidson@gmail.com>
2022-06-27 16:03:21 -0600

    update

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ var fourPm = $("#04pm");
 var fivePm = $("#05pm");
 
 
+// moment().js here
+
+
+
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var currentHour = moment().format('h:mm:ss a');
 
@@ -28,32 +32,32 @@ var currentTimeInterval = setInterval(function() {
 function genTime() {
 
   console.log("Current Hour " + hour);
-  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
-  nineAm.val(init9);
+  var genTime9 = JSON.parse(localStorage.getItem("09:00 am"));
+  nineAm.val(genTime9);
 
-  var init10 = JSON.parse(localStorage.getItem("10:00 am"))
-  tenAm.val(init10);
+  var genTime10 = JSON.parse(localStorage.getItem("10:00 am"))
+  tenAm.val(genTime10);
   
-  var init11 = JSON.parse(localStorage.getItem("11:00 am"))
-  elevenAm.val(init11);
+  var genTime11 = JSON.parse(localStorage.getItem("11:00 am"))
+  elevenAm.val(genTime11);
   
-  var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
-  twelvePm.val(init12);
+  var genTime12 = JSON.parse(localStorage.getItem("12:00 pm"))
+  twelvePm.val(genTime12);
   
-  var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
-  onePm.val(init1);
+  var genTime13 = JSON.parse(localStorage.getItem("01:00 pm"))
+  onePm.val(genTime13);
   
-  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
-  twoPm.val(init2);
+  var genTime14 = JSON.parse(localStorage.getItem("02:00 pm"))
+  twoPm.val(genTime14);
   
-  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
-  threePm.val(init3);
+  var genTime15 = JSON.parse(localStorage.getItem("03:00 pm"))
+  threePm.val(genTime15);
  
-  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
-  fourPm.val(init4);
+  var genTime16 = JSON.parse(localStorage.getItem("04:00 pm"))
+  fourPm.val(genTime16);
   
-  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
-  fivePm.val(init5);
+  var genTime17 = JSON.parse(localStorage.getItem("05:00 pm"))
+  fivePm.val(genTime17);
 } 
 
 function timeProcess () {
